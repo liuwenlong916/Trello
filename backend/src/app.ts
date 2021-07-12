035e38001e92e53ffbfa69f4cc240fe81b30809e
1141657011@qq.com <1141657011@qq.com>
2021-07-12 20:48:32 +0800

    获取数据(params,query,body,header), body需要解析

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,28 +1,29 @@
-import configs from './configs';
-import Koa from 'koa';
-import {bootstrapControllers, Params} from 'koa-ts-controllers'
-import KoaRouter from "koa-router"
+import configs from './configs'
+import Koa from 'koa'
+import { bootstrapControllers, Params } from 'koa-ts-controllers'
+import KoaRouter from 'koa-router'
 import path from 'path'
+import KoaBodyParer from 'koa-bodyparser'
 
-(async ()=>{
-  const app = new Koa();
+;(async () => {
+  const app = new Koa()
 
-  const router = new KoaRouter();
+  const router = new KoaRouter()
 
   //注册路由
   await bootstrapControllers(app, {
     router,
-    basePath:'/api',
-    versions:[1],
-    controllers:[
-      path.resolve(__dirname, 'controllers/**/*')
-    ]
+    basePath: '/api',
+    versions: [1],
+    controllers: [path.resolve(__dirname, 'controllers/**/*')],
   })
 
+  app.use(KoaBodyParer()) //解析body
   app.use(router.routes())
   console.log(router.routes())
-  app.listen(configs.server.port, configs.server.host,()=>{
-    console.log(`服务启动成功：http://${configs.server.host}:${configs.server.port}`)
+  app.listen(configs.server.port, configs.server.host, () => {
+    console.log(
+      `服务启动成功：http://${configs.server.host}:${configs.server.port}`,
+    )
   })
-
 })()
--- backend/src/controllers/Hello.ts
+++ backend/src/controllers/Hello.ts
@@ -1,9 +1,33 @@
-import { Controller, Get } from "koa-ts-controllers";
+import { Controller, Get, Params, Post, Body, Header } from 'koa-ts-controllers'
 
-@Controller('/hello')
-class HelloController{
+@Controller('/hello') //类装饰器
+class HelloController {
   @Get('/world')
-  async sayWorld(){
+  async sayWorld() {
     return 'Hello world'
+  }
+  @Get('/user/:id') //方法装饰器
+  async user(
+    @Params('id') //形参装饰器
+    id: number,
+  ) {
+    return `当前params中的id是${id}`
+  }
+  //Query
+  //body 需要安装 koa-bodyparser
+  //header
+
+  @Post('user')
+  async postUser(
+    @Body()
+    body: {
+      name: string
+      password: string
+    },
+    @Header()
+    h: any,
+  ) {
+    console.log(`头信息：${h}`)
+    return `当前提交的body数据是：${JSON.stringify(body)}`
   }
 }
