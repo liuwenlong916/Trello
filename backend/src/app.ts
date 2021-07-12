7cb9610f227b935f618c6bdc0241f4ef60c991ed
1141657011@qq.com <1141657011@qq.com>
2021-07-12 20:53:23 +0800

    统一错误处理

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,9 @@
 import configs from './configs'
-import Koa from 'koa'
+import Koa, { Context } from 'koa'
 import { bootstrapControllers, Params } from 'koa-ts-controllers'
 import KoaRouter from 'koa-router'
 import path from 'path'
 import KoaBodyParer from 'koa-bodyparser'
-
 ;(async () => {
   const app = new Koa()
 
@@ -16,6 +15,17 @@ import KoaBodyParer from 'koa-bodyparser'
     basePath: '/api',
     versions: [1],
     controllers: [path.resolve(__dirname, 'controllers/**/*')],
+    //统一错误处理
+    errorHandler: async (err: any, ctx: Context) => {
+      let status = 500
+      let body = {
+        statusCode: status,
+        error: 'Internal Server error',
+        message: 'An internal server error occurred',
+      }
+      ctx.body = body
+      ctx.status = status
+    },
   })
 
   app.use(KoaBodyParer()) //解析body
