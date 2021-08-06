0a0c0f2e16080d8e6da39705058a1ca4aac5434d
liuwenlong916 <1141657011@qq.com>
2021-08-06 16:29:41 +0800

    提示 动画

--- frontend/vueapp/src/components/KForm/KFormItem.vue
+++ frontend/vueapp/src/components/KForm/KFormItem.vue
@@ -1,8 +1,12 @@
 <template>
-  <div>
+  <div class="item">
     <label v-if="label">{{ label }}</label>
     <slot></slot>
-    <p v-if="error" style="color:red">{{ error }}</p>
+    <div style="height:20px">
+      <transition name="slide-fade">
+        <p v-if="error" style="color:red; ">{{ error }}</p>
+      </transition>
+    </div>
   </div>
 </template>
 
@@ -49,4 +53,18 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.slide-fade-enter-active {
+  transition: all 0.3s ease;
+}
+.slide-fade-leave-active {
+  transition: all 0.1s cubic-bezier(1, 0.5, 0.8, 1);
+}
+/* .slide-fade-leave-active 用于 2.1.8 以下版本 */
+.slide-fade-enter,
+.slide-fade-leave-to {
+  /* transform: translateX(10); */
+  opacity: 0;
+  /* max-height: 20px; */
+}
+</style>
